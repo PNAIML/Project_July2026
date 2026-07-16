d52c76d670aab363e9cde7330704980271d27c9d
PNAIML <punit541985@gmail.com>
2026-07-16 17:11:52 +0530

    Fix exception handling and improve MongoDB connection

--- database/mongodb.py
+++ database/mongodb.py
@@ -3,13 +3,13 @@ import certifi
 try:
     MONGO_URI = "mongodb+srv://punitectssrgsp_db_user:mongodb123@cluster0.h7lbrs1.mongodb.net/"
     client = MongoClient(MONGO_URI,tls=True,tlsCAFile=certifi.where())
-    db = client.admit.command("ping")
+    client.admit.command("ping")
     db = client["ssus1234"]
     students_collection = db["students"]
     marks_collection = db["marks"]
     attendance_collection = db["attendance"]
     bmi_collection = db["bmi_reports"]
     print("MongoDB connection successfully")
-except ExceptionGroup as e:
+except Exception as e:
     print("MongoDB Error:",e)
 #MONGODB CODE
