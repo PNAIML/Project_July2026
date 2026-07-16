137866df48c450aa110e39fe164ccb2c4f5847f5
PNAIML <punit541985@gmail.com>
2026-07-16 17:03:40 +0530

    Update mongodb.py

--- database/mongodb.py
+++ database/mongodb.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
+import certifi
 try:
     MONGO_URI = "mongodb+srv://punitectssrgsp_db_user:MongoDB#05@cluster0.h7lbrs1.mongodb.net/"
-    client = MongoClient(MONGO_URI)
+    client = MongoClient(MONGO_URI,tls=True,tlsCAFile=certifi.where()
     db = client.admit.command("ping")
     db = client["ssus1234"]
     students_collection = db["students"]
