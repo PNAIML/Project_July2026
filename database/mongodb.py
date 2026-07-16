45395c5e16bc98bbc326977760660a646e958267
PNAIML <punit541985@gmail.com>
2026-07-16 17:05:03 +0530

    Fix indentation in MongoDB connection code

--- database/mongodb.py
+++ database/mongodb.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import certifi
 try:
     MONGO_URI = "mongodb+srv://punitectssrgsp_db_user:MongoDB#05@cluster0.h7lbrs1.mongodb.net/"
-    client = MongoClient(MONGO_URI,tls=True,tlsCAFile=certifi.where()
+    client = MongoClient(MONGO_URI,tls=True,tlsCAFile=certifi.where())
     db = client.admit.command("ping")
     db = client["ssus1234"]
     students_collection = db["students"]
